8eaba5ce4b67bf99ba26b651569abfd5dd8cd0ec
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-10-10 11:14:58 +0800

    Comment out paths

--- django_crud_api/django_crud_api/urls.py
+++ django_crud_api/django_crud_api/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 from django.conf.urls import url, include
 from rest_framework.documentation import include_docs_urls
 
 from django_crud_api import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 
     url(r'^admin/', admin.site.urls),
     url(r'^docs/', include_docs_urls(title='Todo API', description='RESTful API for Todo')),
