b306a1ec5644238050ef63b0f7179a1654d9b18b
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-10-10 11:13:00 +0800

    Add URL routes

--- django_crud_api/django_crud_api/urls.py
+++ django_crud_api/django_crud_api/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.documentation import include_docs_urls
+
+from django_crud_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^docs/', include_docs_urls(title='Todo API', description='RESTful API for Todo')),
+ 
+    url(r'^$', views.api_root),
+    url(r'^', include('users.urls', namespace='users')),
+    url(r'^', include('todos.urls', namespace='todos')),
 ]
